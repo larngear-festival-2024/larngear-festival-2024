9762f7bcc2d5a6ac7e9e2baaa307032ae5ea19cf
teegoood <teegooodgood@gmail.com>
2024-10-13 20:47:31 +0700

    fix: add defualt value for size props

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 /**
  * Logo component that displays an image with a specified size.
- * @param {number} size - The size of the logo image (width) (for height will be calculated from aspect ratio) .
+ * @param {number} size - The width of the logo image (px) (for height will be calculated from aspect ratio) .
  */
 
-export default function Logo({ size }: { size: number }) {
+export default function Logo({ size = 200 }: { size?: number }) {
     return <Image src="logo.svg" alt="logo" width={size} height={size} />;
 }
