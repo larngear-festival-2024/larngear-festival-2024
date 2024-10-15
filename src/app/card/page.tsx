74dbbca59cb0dde475f0b25acd48f446ceb6334c
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-16 02:22:50 +0700

    fix: ui design

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Image from 'next/image';
+import Border from '@/components/border';
 import Logo from '@public/logo.svg';
 import BG from '@public/card/bg.svg';
 import Sticker from '@/components/card/Sticker';
@@ -14,18 +15,17 @@ import Stamp4 from '@public/card/stamp4.svg';
 import Stamp5 from '@public/card/stamp5.svg';
 import Stamp6 from '@public/card/stamp6.svg';
 
-const Stamps = [Stamp1, Stamp2, Stamp3, Stamp4, Stamp5, Stamp6];
-
 type CardMode = 'sticker' | 'name' | 'share';
+
+const Stamps = [Stamp1, Stamp2, Stamp3, Stamp4, Stamp5, Stamp6];
+const phases: CardMode[] = ['sticker', 'name', 'share'];
+const rotations = [-15, 0, 15, -15, 0, 15];
 
 export default function Card() {
     const [phase, setPhase] = useState<CardMode>('sticker');
     const [selected, setSelected] = useState<number | null>(null);
     const [name, setName] = useState<string>('');
     const [stamps, setStamps] = useState<string[]>(['', '', '', '', '', '']);
-
-    const phases: CardMode[] = ['sticker', 'name', 'share'];
-    const rotations = [-15, 0, 15, -15, 0, 15];
 
     const handleNextPhase = () => {
         setPhase((prevPhase) => {
@@ -46,60 +46,67 @@ export default function Card() {
     };
 
     return (
-        <main className="flex flex-col items-center justify-center gap-12 bg-purple-800 p-10">
-            <Image src={Logo} alt="logo" width={100} height={100} />
-            <section className="relative h-[195.39px] w-full max-w-[300px]">
+        <Border>
+            <main className="flex flex-col items-center justify-center gap-9 p-2">
                 <Image
-                    src={BG}
-                    alt="background"
-                    layout="fill"
-                    objectFit="cover"
-                    className="absolute left-0 top-0"
+                    src={Logo}
+                    alt="logo"
+                    width={phase === 'share' ? 158 : 84.54}
+                    height={phase === 'share' ? 176 : 94.38}
                 />
+                <section className="relative h-[195.39px] w-full max-w-[300px]">
+                    <Image
+                        src={BG}
+                        alt="background"
+                        layout="fill"
+                        objectFit="cover"
+                        className="absolute left-0 top-0"
+                    />
 
-                <div className="z-1 relative grid h-full w-full grid-cols-3 grid-rows-2">
-                    {stamps.map((Stamp, index) => (
-                        <div
-                            key={index}
-                            className={`grid place-items-center ${selected === index && phase === 'sticker' ? 'animate-pulse border-2 border-purple-500' : ''}`}
-                            onClick={() => setSelected(index)}
-                        >
-                            {Stamp !== '' && (
-                                <Image
-                                    src={Stamp}
-                                    className="h-4/5 w-4/5 overflow-hidden object-contain"
-                                    alt={`Stamp ${index + 1}`}
-                                    style={{
-                                        transform: `rotate(${rotations[index]}deg)`,
-                                    }}
-                                />
-                            )}
-                        </div>
-                    ))}
-                </div>
-            </section>
-            {phase === 'sticker' && (
-                <Sticker
-                    stickers={Stamps}
-                    selected={selected}
-                    handleSelected={setSelected}
-                    handleStamps={handleSelectedStamp}
-                />
-            )}
-            {phase === 'name' && (
-                <Name
-                    name={name}
-                    handleKeyPressed={handleKeyPress}
-                    handleNameChanged={setName}
-                />
-            )}
-            {phase === 'share' && <Share name={name} />}
-            <button
-                onClick={handleNextPhase}
-                className={`h-12 w-64 rounded-2xl border-2 border-black ${phase !== 'share' ? 'bg-blue-600' : 'bg-pink-400'} text-3xl text-white`}
-            >
-                {phase !== 'share' ? 'ไปกันต่อ!' : 'แชร์ผลลัพธ์'}
-            </button>
-        </main>
+                    <div className="z-1 relative grid h-full w-full grid-cols-3 grid-rows-2">
+                        {stamps.map((Stamp, index) => (
+                            <div
+                                key={index}
+                                className={`grid place-items-center ${selected === index && phase === 'sticker' ? 'animate-pulse border-2 border-purple-500' : ''}`}
+                                onClick={() => setSelected(index)}
+                            >
+                                {Stamp !== '' && (
+                                    <Image
+                                        src={Stamp}
+                                        className="h-4/5 w-4/5 overflow-hidden object-contain"
+                                        alt={`Stamp ${index + 1}`}
+                                        style={{
+                                            transform: `rotate(${rotations[index]}deg)`,
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                {phase === 'sticker' && (
+                    <Sticker
+                        stickers={Stamps}
+                        selected={selected}
+                        handleSelected={setSelected}
+                        handleStamps={handleSelectedStamp}
+                    />
+                )}
+                {phase === 'name' && (
+                    <Name
+                        name={name}
+                        handleKeyPressed={handleKeyPress}
+                        handleNameChanged={setName}
+                    />
+                )}
+                {phase === 'share' && <Share name={name} />}
+                <button
+                    onClick={handleNextPhase}
+                    className={`absolute bottom-20 h-12 w-64 rounded-lg border-2 border-black ${phase !== 'share' ? 'bg-project-light-blue' : 'bg-project-red-orange'} text-3xl text-white`}
+                >
+                    {phase !== 'share' ? 'ไปกันต่อ!' : 'แชร์ผลลัพธ์'}
+                </button>
+            </main>
+        </Border>
     );
 }
--- src/components/card/Name.tsx
+++ src/components/card/Name.tsx
@@ -10,13 +10,16 @@ export default function Name(props: NameProps) {
     const { name, handleKeyPressed, handleNameChanged } = props;
 
     return (
-        <input
-            type="text"
-            value={name}
-            onChange={(e) => handleNameChanged(e.target.value)}
-            onKeyPress={handleKeyPressed}
-            className="h-9 w-72 rounded-2xl border-2 border-black p-2 text-center"
-            placeholder="ชื่อของคุณ"
-        />
+        <div className="flex flex-col items-center gap-8">
+            <h1 className="text-3xl text-white">บัตรนี้แสดงตนถึง</h1>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => handleNameChanged(e.target.value)}
+                onKeyPress={handleKeyPressed}
+                className="h-9 w-72 rounded-xl border-2 border-black p-2 text-center text-2xl"
+                placeholder="ชื่อของคุณ"
+            />
+        </div>
     );
 }
