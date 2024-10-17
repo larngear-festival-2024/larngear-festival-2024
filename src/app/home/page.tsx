4d281c073922a0fc2ec6677067af963fa2a5af4d
Thiraput01 <Thiraputball@gmail.com>
2024-10-17 08:36:39 +0700

    feat: countdown logic

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,62 @@
+'use client';
 import Button from '@/components/button';
 import Logo from '@/components/logo';
 import Tape from '@/components/tape';
 import TimerCard from '@/components/timercard';
+import { log } from 'console';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
+const OPENING_TIME = +new Date(2024, 9, 18, 0, 0, 0, 0);
+
+interface TimeLeft {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+}
+
+const calculateTimeLeft: (time: number) => TimeLeft | null = (time) => {
+    const timeDiff = time - +new Date();
+
+    const dayLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hourLeft = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+    const minuteLeft = Math.floor((timeDiff / 1000 / 60) % 60);
+    const secondLeft = Math.floor((timeDiff / 1000) % 60);
+
+    console.log(dayLeft);
+    if (timeDiff > 0) {
+        return {
+            days: dayLeft,
+            hours: hourLeft,
+            minutes: minuteLeft,
+            seconds: secondLeft,
+        };
+    } else {
+        return null;
+    }
+};
+
+export default function Home({ until = OPENING_TIME }: { until?: number }) {
+    const [timeLeft, setTimeLeft] = useState<TimeLeft | null>(
+        calculateTimeLeft(until)
+    );
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeLeft(calculateTimeLeft(until));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [until]);
+
     return (
-        <div className="flex h-screen w-full flex-col items-center justify-between">
+        <div className="flex h-screen w-full flex-col items-center justify-between overflow-y-auto">
             {/* Main */}
-            <div className="mb-[71px] mt-[64px] flex h-[709px] w-[347px] flex-grow flex-col items-center justify-start border-2">
-                <div className="border-3 mt-4 flex justify-center">
+            <div className="mb-[71px] mt-[64px] flex h-[709px] w-[347px] flex-grow flex-col items-center justify-start">
+                <div className="mt-4 flex justify-center">
                     <Logo />
                 </div>
-                <div className="relative flex h-[211px] w-[307px] flex-col items-center border-2 p-[32px]">
+                <div className="relative flex h-[211px] w-[307px] flex-col items-center p-[32px]">
                     <div className="absolute">
                         <Tape size={300} />
                     </div>
@@ -24,21 +69,32 @@ export default function Home() {
 
                 <div className="flex flex-col items-center">
                     <div className="mt-8 flex flex-row space-x-4">
-                        <TimerCard bgClass="bg-project-light-blue" text="00" />
-                        <TimerCard bgClass="bg-project-yellow" text="00" />
-                        <TimerCard bgClass="bg-project-pink" text="00" />
-                        <TimerCard bgClass="bg-project-dark-green" text="00" />
+                        <TimerCard
+                            bgClass="bg-project-light-blue"
+                            num={timeLeft?.days ?? 0}
+                        />
+                        <TimerCard
+                            bgClass="bg-project-yellow"
+                            num={timeLeft?.hours ?? 0}
+                        />
+                        <TimerCard
+                            bgClass="bg-project-pink"
+                            num={timeLeft?.minutes ?? 0}
+                        />
+                        <TimerCard
+                            bgClass="bg-project-dark-green"
+                            num={timeLeft?.seconds ?? 0}
+                        />
                     </div>
-
-                    <div className="mt-4 flex h-[32px] w-[283px] bg-project-yellow">
-                        hms
+                    <div className="text-1xl mt-8 flex h-[32px] w-[283px] items-center justify-center rounded-md bg-project-yellow text-center font-sov text-black">
+                        DAYS : HOURS : MINUTES : SECONDS
                     </div>
                 </div>
             </div>
 
             {/* Footer */}
-            <div className="inline-flex h-[52px] w-full flex-col items-start justify-start gap-2.5 rounded-tl-lg rounded-tr-lg bg-project-yellow px-[104px] py-4">
-                footer
+            <div className="text-1xl flex h-[52px] w-full flex-col items-center justify-center gap-2.5 rounded-tl-lg rounded-tr-lg bg-project-yellow px-[104px] py-4 font-sov text-black">
+                [icon] @larngearfest2024
             </div>
         </div>
     );
