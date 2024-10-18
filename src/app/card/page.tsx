cba88594f3b990c62bc77dfbc7574693db1f7bb2
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-18 15:34:02 +0700

    feat: add more stickers

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -1,23 +1,43 @@
 'use client';
-import React from 'react';
-import { useState } from 'react';
-import Image from 'next/image';
 import Border from '@/components/border';
-import Logo from '@public/logo.svg';
-import BG from '@public/card/bg.svg';
-import Sticker from '@/components/card/Sticker';
 import Name from '@/components/card/Name';
 import Share from '@/components/card/Share';
+import Sticker from '@/components/card/Sticker';
+import BG from '@public/card/bg.svg';
 import Stamp1 from '@public/card/stamp1.svg';
 import Stamp2 from '@public/card/stamp2.svg';
 import Stamp3 from '@public/card/stamp3.svg';
 import Stamp4 from '@public/card/stamp4.svg';
 import Stamp5 from '@public/card/stamp5.svg';
 import Stamp6 from '@public/card/stamp6.svg';
+import Stamp7 from '@public/card/stamp7.svg';
+import Stamp8 from '@public/card/stamp8.svg';
+import Stamp9 from '@public/card/stamp9.svg';
+import Stamp10 from '@public/card/stamp10.svg';
+import Stamp11 from '@public/card/stamp11.svg';
+import Stamp12 from '@public/card/stamp12.svg';
+import Stamp13 from '@public/card/stamp13.svg';
+import Logo from '@public/logo.svg';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 type CardMode = 'sticker' | 'name' | 'share';
 
-const Stamps = [Stamp1, Stamp2, Stamp3, Stamp4, Stamp5, Stamp6];
+const Stamps = [
+    Stamp1,
+    Stamp2,
+    Stamp3,
+    Stamp4,
+    Stamp5,
+    Stamp6,
+    Stamp7,
+    Stamp8,
+    Stamp9,
+    Stamp10,
+    Stamp11,
+    Stamp12,
+    Stamp13,
+];
 const phases: CardMode[] = ['sticker', 'name', 'share'];
 const rotations = [-15, 0, 15, -15, 0, 15];
 
