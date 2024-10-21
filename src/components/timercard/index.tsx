6212cf7c895bf01ec06ae9baa6151e62d73ea928
Thiraput01 <Thiraputball@gmail.com>
2024-10-21 20:50:39 +0700

    fix: use cn()

--- src/components/timercard/index.tsx
+++ src/components/timercard/index.tsx
@@ -1,3 +1,5 @@
+import cn from 'classnames';
+
 interface TimerCardProps {
     bgClass: string;
     text: string;
@@ -6,7 +8,10 @@ interface TimerCardProps {
 export default function TimerCard({ bgClass, text }: TimerCardProps) {
     return (
         <div
-            className={`inline-flex h-[104px] w-[72px] flex-col items-center justify-center gap-2.5 border-2 border-black py-1 shadow drop-shadow-2xl ${bgClass}`}
+            className={cn(
+                'inline-flex h-[104px] w-[72px] flex-col items-center justify-center gap-2.5 border-2 border-black py-1 shadow drop-shadow-2xl',
+                bgClass
+            )}
         >
             <div className="flex h-full w-full items-center justify-center text-center font-bangkok text-6xl text-black">
                 {text}
