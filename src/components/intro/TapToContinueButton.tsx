ecc2e71002ba4ca63b15445418982cca5f033ab0
teegoood <teegooodgood@gmail.com>
2024-10-29 17:32:14 +0700

    style: fix layout shift

--- src/components/intro/TapToContinueButton.tsx
+++ src/components/intro/TapToContinueButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { ChevronRight } from 'lucide-react';
 import React from 'react';
 
@@ -11,15 +12,20 @@ const TapToContinueButton: React.FC<TapToContinueButtonProps> = ({
     showTapToContinue,
 }) => {
     return (
-        showTapToContinue && (
-            <div className="flex w-full animate-fade-in-bounce items-center justify-end space-x-1">
-                <div className="flex flex-col items-center font-sov text-base font-normal text-white">
-                    <p>Tap to</p>
-                    <p>continue</p>
-                </div>
-                <ChevronRight size={33} color="white" />
+        <div
+            className={cn(
+                'invisible flex w-full animate-fade-in-bounce items-center justify-end space-x-1',
+                {
+                    visible: showTapToContinue,
+                }
+            )}
+        >
+            <div className="flex flex-col items-center font-sov text-base font-normal text-white">
+                <p>Tap to</p>
+                <p>continue</p>
             </div>
-        )
+            <ChevronRight size={33} color="white" />
+        </div>
     );
 };
 
